affcc983aa382232a072c52467cf68427f5f13f1
b-bista <44533657+b-bista@users.noreply.github.com>
2019-12-28 01:03:08 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,17 @@ function calcTip(ordSub, tRate){
   return ordSub * tRate;
 }
 function calcResult(){
-  var orderSubtotal=document.forms["tipForm"]["orderSubtotal"].value;
-  var nSplit=document.forms["tipForm"]["nSplit"].value;
-  var tipRate=document.forms["tipForm"]["tipRate"].value;
-  
+  var orderSubtotal=parseFloat(document.forms["tipForm"]["orderSubtotal"].value);
+  var nSplit=parseFloat(document.forms["tipForm"]["nSplit"].value);
+  var tipRate=parseFloat(document.forms["tipForm"]["tipRate"].value);
+
   var orderTotal=orderSubtotal + calcTip(orderSubtotal, tipRate);
   var tipTotal=calcTip(orderSubtotal, tipRate);
   var splitTotal=orderTotal/nSplit;
-  
-  var result = ("The total bill with tip is: " + orderTotal + 
-                "\nThe total tip amount is: " + tipTotal +
-               "\nThe total amount owed per person is: " +splitTotal);
-  
+
+  var result = ("The total bill with tip is: " + (orderTotal.toFixed(2)) +
+                ". The total tip amount is: " + (tipTotal.toFixed(2)) +
+               ". The total amount owed per person is: " + (splitTotal.toFixed(2)) );
+
   document.getElementById("result").innerHTML = result;
 }
